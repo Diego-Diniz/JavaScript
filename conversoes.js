89b4e35eccf84abf22b7b88bf60a9f7755b6eb88
Diego Henrique Diniz <115659288+Diego-Diniz@users.noreply.github.com>
2023-09-02 22:05:04 -0300

    codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,13 @@
+// tipo de dados
+// booleanos
+
+// conversão implicita
+
+const numero = 456;
+const numeroString = "456";
+
+
+console.log(numero + Number(numeroString));
+
+
+// conversão explícita
